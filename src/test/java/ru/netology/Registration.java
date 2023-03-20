5ce217f94f9d976040e1f8d3c014f8a82963b675
Luyba <lsorlova82@gmail.com>
2023-03-20 19:46:06 +0200

    tests were added

---
+++ src/test/java/ru/netology/Registration.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+    public class Registration {
+        private String login;
+        private String password;
+        private String status;
+
+}
