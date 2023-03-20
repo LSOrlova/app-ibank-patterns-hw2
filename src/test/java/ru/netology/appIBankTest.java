a2fc37474dff6ac791849da9eb4764063104b4fd
Luyba <lsorlova82@gmail.com>
2023-03-20 18:55:59 +0200

    initial commit

---
+++ src/test/java/ru/netology/appIBankTest.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class appIBankTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
